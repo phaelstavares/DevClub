9d9e75ba2a7f657b462c4c7078255bf14ae743cd
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-01 18:03:12 -0300

    boolean

--- JavaScript/03-tiposDeDados.js
+++ JavaScript/03-tiposDeDados.js
@@ -2,11 +2,13 @@
     Tipos de Dados:
     1. String -> Textos
     2. Numbers -> Números
+    3. Boolean -> Boleano
 
     Operadores aritméticos:
     + - * /
 */
 
+// String
 const texto1 = "Textos com aspas duplas"
 const texto2 = 'Textos com aspas simples'
 const texto3 = `textos com a crase` // Template literals ou Template string
@@ -14,7 +16,7 @@ console.log(texto1)
 console.log(texto2)
 console.log(texto3)
 
-
+// Numbers e String
 const myIdade = 18
 const myCar = 'carro'
 const myString = `Minha idade é ${myIdade}, já sou adulto e agora estou estudando e futuramente estarei 
@@ -23,6 +25,7 @@ andando de ${myCar}.`
 console.log(myString)
 
 
+// Numbers
 const numero1 = 3.126 * 2
 const fakeNumero = "32 / 4" // aspas conta como textos
 console.log(numero1)
