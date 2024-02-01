ffb6ba8de4bf04b26528533533eed745458974af
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-01 16:58:02 -0300

    string e tipos de dados

---
+++ JavaScript/03-tiposDeDados.js
@@ -0,0 +1,21 @@
+/*
+    1. String -> Textos
+*/
+
+const texto1 = "Textos com aspas duplas"
+const texto2 = 'Textos com aspas simples'
+const texto3 = `textos com a crase` // Template literals ou Template string
+
+console.log(texto1)
+console.log(texto2)
+console.log(texto3)
+
+
+
+const myIdade = 18
+const myCar = 'carro'
+const myString = `Minha idade é ${myIdade}, já sou adulto e agora estou estudando e futuramente estarei 
+andando de ${myCar}.`
+// ${} é código js e com crase eu posso pular linha
+
+console.log(myString)
