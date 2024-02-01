00b73630ed96b8be204419323f17a04f8a396ff8
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-01 15:11:39 -0300

    variáveis

---
+++ JavaScript/02-Variáveis.js
@@ -0,0 +1,13 @@
+/*
+    Variáveis:
+    - let -> Pode alterar o valor quando quiser
+    - const -> Valor NÃO pode ser declarado NOVAMENTE
+    - var -> DESCONTINUADO / NÃO USAR
+
+    variável é um pedaço de memória que está sendo guardado
+*/
+
+const numeroDeAlunosDaSalaSete = 40
+// não comece com números e nem espaçamento ao nomear uma variável.
+
+console.log(numeroDeAlunosDaSalaSete)
