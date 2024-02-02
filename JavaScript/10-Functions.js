6013bd6997dd972a5720bb9e0feddd1c062c128e
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-02 15:34:51 -0300

    functions

---
+++ JavaScript/10-Functions.js
@@ -0,0 +1,18 @@
+/*
+    Functions -> Funções
+    - Trecho de código que é executado
+    - SOMENTE quando solicitado
+*/
+
+const variavel = "abacatinho"
+
+console.log(variavel)
+
+
+
+function nomeNaTela() {
+    console.log("Rodolfo")
+}
+
+nomeNaTela()
+// só vai executar quando ela for chamada
