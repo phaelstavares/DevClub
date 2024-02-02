299e930716f861fa3efd0e87cef65541d53814be
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-02 14:51:26 -0300

    array

---
+++ JavaScript/08-array.js
@@ -0,0 +1,28 @@
+/*
+    Estrutura de Dados:
+    1. Array -> Guarda muitas informações
+*/
+
+const users = [
+    {
+        nome: "Raphael",
+        idade: 18,
+        nomeDoCasal: null,
+        id: null
+    },
+    {
+        nome: "Aline",
+        idade: 25,
+        nomeDoCasal: "Rodolfo",
+        id: null
+    },
+]
+
+
+const myArray = [20, 30, 240]
+//     posição:  0   1   2
+myArray[2] = 271
+// Trocando o 240 por 271
+
+console.log(myArray[0])
+console.log(myArray)
