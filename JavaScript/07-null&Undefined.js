c8169cdc47649d43fe4d345872dc3f80b68be171
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-02 13:55:37 -0300

    null & undefined

---
+++ JavaScript/07-null&Undefined.js
@@ -0,0 +1,23 @@
+/*
+    Tipos de Dados:
+    1. String -> Textos
+    2. Numbers -> Números
+    3. Boolean -> Boleano
+    4. Object -> Objetos
+    5. Null & Undefined
+
+    Operadores aritméticos:
+    + - * /
+*/
+
+const user = {
+    nome: "Raphael",
+    idade: 18,
+    nomeDoCasal: null,
+    id: null
+}
+
+user.id = 348624
+
+console.log(user.id)
+// Undefined é quando não é encontrado
