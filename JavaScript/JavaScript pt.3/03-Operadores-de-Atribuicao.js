66eb875644894e4fade2fdc49c86fc360ebd8e9f
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-07 20:43:40 -0300

    operadores de atribuição

---
+++ JavaScript/JavaScript pt.3/03-Operadores-de-Atribuicao.js
@@ -0,0 +1,39 @@
+/*
+    Operadores de Atribuição -> Atalhos para economizar código
+
+    =   ->  Atribuição
+    +=  ->  Adição
+    -=  ->  Subtração
+    /=  ->  Divisão
+    *=  ->  Multiplicação
+    %=  ->  Resto
+
+*/
+
+const result1 = 20  // Atribuição
+console.log(result1)
+
+
+let result2 = 20
+result2 += 70       // result2 = result2 + 70
+console.log(result2)
+
+
+let result3 = 37
+result3 -= 36       // result3 = result3 - 36
+console.log(result3)
+
+
+let result4 = 16
+result4 /= 2        // result4 = result4 / 2
+console.log(result4)
+
+
+let result5 = 20
+result5 *= 2        // result5 = result5 * 2
+console.log(result5)
+
+
+let result6 = 13
+result6 %= 2        // result6 = result6 % 2
+console.log(result6)
