793ab5b9bc0fb0a0ff65abee79eaaae4ecdf43fc
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-07 15:00:42 -0300

    Math

---
+++ JavaScript/JavaScript pt.3/01-Math.js
@@ -0,0 +1,34 @@
+/*
+    Math -> Matemática
+
+    - pow(2,2) / potência / 2² = 4
+    - sqrt(25,2) / raiz / √25
+    - PI / 3,14
+    - ceil / teto -> arredondar pra cima
+    - floor / chão -> arredondar pra baixo
+
+    - random() / número aleatório entre 0 e 1
+*/
+
+const result1 = Math.pow(6,2) // 6²
+console.log(result1)
+
+
+const result2 = Math.sqrt(25,2) // √25
+console.log(result2)
+
+
+const result3 = Math.PI // PI 3,14
+console.log(result3)
+
+
+const result4 = Math.ceil(3.14) // arredondar pra cima
+console.log(result4)
+
+
+const result5 = Math.floor(3.14) // arredondar pra baixo
+console.log(result5)
+
+
+const result6 = Math.random() // número aleatório entre 0 e 1
+console.log(result6)
