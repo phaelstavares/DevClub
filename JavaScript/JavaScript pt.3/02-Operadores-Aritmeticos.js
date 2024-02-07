2a5c344754e7696cb6f5fb34c3da709eb4ec165f
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-07 20:10:30 -0300

    operadores aritméticos

---
+++ JavaScript/JavaScript pt.3/02-Operadores-Aritmeticos.js
@@ -0,0 +1,45 @@
+/*
+    Operadores Aritméticos
+
+    +   ->  Adição
+    -   ->  Subtração
+    /   ->  Divisão
+    *   ->  Multiplicação
+    
+    %   ->  Resto
+    ++  ->  Incremento
+    --  ->  Decremento
+    **  ->  Exponencial
+
+*/
+
+const result1 = 2 + 3
+console.log(result1)
+
+const result2 = 5 - 8
+console.log(result2)
+
+const result3 = 70 / 5
+console.log(result3)
+
+const result4 = 6 * 6
+console.log(result4)
+
+const result5 = 25 % 5 // Resto da divisão (trás o que sobrou)
+console.log(result5)
+
+
+let result6 = 20
+result6++
+result6++
+console.log(result6) // resultado: 20 + 1 + 1
+
+
+let result7 = 20
+result7--
+result7--
+console.log(result7) // resultado: 20 - 1 - 1
+
+
+let result8 = 6 ** 2 // 2² parecido com o 'pow(2,2)'
+console.log(result8)
