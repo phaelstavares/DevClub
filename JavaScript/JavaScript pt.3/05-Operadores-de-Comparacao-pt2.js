4a576972e28792edcf0d0440e49a50295ce2f7b3
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-08 14:24:17 -0300

    operadores de comparação pt.2

---
+++ JavaScript/JavaScript pt.3/05-Operadores-de-Comparacao-pt2.js
@@ -0,0 +1,24 @@
+/*
+    Operadores de comparação
+
+    >   ->   Maior
+    <   ->   Menor
+    ==  ->   Igual
+
+    >=  ->   Maior ou Igual
+    <=  ->   Menor ou Igual
+*/
+
+const primeiroNumero = 20
+const segundoNumero = 30
+const terceiroNumero = 20
+
+console.log(primeiroNumero > segundoNumero) // 20 é MAIOR que 30?
+
+console.log(primeiroNumero < segundoNumero) // 20 é MENOR que 30?
+
+console.log(primeiroNumero == terceiroNumero) // 20 é IGUAL a 20?
+
+console.log(primeiroNumero >= terceiroNumero) // É IGUAL
+
+console.log(primeiroNumero <= segundoNumero) // É MENOR
