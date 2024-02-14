3b8330d6664eeeac33d4f72618f4b28ce427de88
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-14 17:33:53 -0300

    for of

---
+++ JavaScript/JavaScript pt.4/02-For-of.js
@@ -0,0 +1,16 @@
+/*
+    Estrutura de Repetição - LOOP
+    For of
+*/
+
+const myName = "Raphael"
+const users = ["Maria", "Rodrigo", "Alexandra", "Felipe"]
+
+for (const letra of myName) {
+    console.log(letra)
+}
+
+
+for (const nome of users) {
+    console.log(nome)
+}
