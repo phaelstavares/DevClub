42f513e9008df8ccb92d45564a403bab559a6be3
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-15 15:58:43 -0300

    do while

---
+++ JavaScript/JavaScript pt.4/04-Do-while.js
@@ -0,0 +1,26 @@
+/*
+    while & do while
+
+    while     ->  Verifique se é TRUE e faça
+    do while  ->  Faça, depois veja se é TRUE
+*/
+
+let i = 0
+
+/*
+
+console.log("Um código aqui")
+
+while (i < 30) {
+    i++;
+    console.log(i)
+}
+
+console.log("Depois do while")
+
+*/
+
+do {
+    i++;
+    console.log(i)
+} while (i < 10);
