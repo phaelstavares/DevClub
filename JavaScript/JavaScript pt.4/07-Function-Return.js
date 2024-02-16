f431c8b1789367c88eb35a419725bcfece770dde
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-16 17:31:08 -0300

    function return

---
+++ JavaScript/JavaScript pt.4/07-Function-Return.js
@@ -0,0 +1,26 @@
+/*
+    Função void (vazia), não retorna nada.
+    Função return, retorna um valor para quem chamou a função.
+
+    'return' tem que ser o último elemento, quando a função encontra o 'return' ela para de funcionar.
+*/
+
+// void
+function soma(valor1, valor2){
+    console.log(valor1 + valor2)
+}
+
+const result = soma(10, 20)
+console.log(result, " Resultado") // não retorna
+
+
+
+// return
+function sob(valor1, valor2){
+    return valor1 + valor2
+    // 'return' tem que ser o último elemento, quando a função encontra o 'return' ela para de funcionar.
+}
+
+console.log(sob(10, 70), " é o resultado.") // retorna
+
+console.log("\n") // pular linha no terminal
