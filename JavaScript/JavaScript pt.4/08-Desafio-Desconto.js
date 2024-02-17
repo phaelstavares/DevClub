fa663205c02de35068e6efbcdd98ce5e0cef5d6f
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-16 21:59:31 -0300

    desafio calcular desconto

---
+++ JavaScript/JavaScript pt.4/08-Desafio-Desconto.js
@@ -0,0 +1,37 @@
+/*
+    Calcular Desconto:
+    Todos os produtos acima de R$ 30,00 tem desconto de 10%
+
+    R$ 250,00 * 7
+    1750 / 100
+    17,50
+*/
+
+const cart = [250, 147, 25, 5, 289, 367]
+let valorFinal = 0
+let valorTotal = 0
+
+function calcularDesconto(preco, desconto){
+    const resultado = (preco * desconto) / 100
+    return resultado
+}
+
+function calcularTotal(){
+}
+
+cart.forEach(valortotal => {
+    calcularTotal(valortotal)
+    valorTotal = valorTotal + valortotal
+});
+
+cart.forEach(valor => {
+    if(valor > 30){
+        const desconto = calcularDesconto(valor, 10)
+        valorFinal = valorFinal + (valor - desconto)
+    } else {
+        valorFinal += valor // valorFinal = valorFinal + valor
+    }
+});
+
+
+console.log(`O valor final da compra foi R$ ${valorTotal}, porém você teve desconto de 10% e ficou R$ ${valorFinal}.`)
