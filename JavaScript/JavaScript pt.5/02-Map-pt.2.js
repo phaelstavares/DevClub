359dc8afe507e7bc348cffd5313a99aab1d907bf
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-21 20:39:56 -0300

    Map pt.2 e desafio

--- JavaScript/JavaScript pt.5/02-Map-pt.2.js
+++ JavaScript/JavaScript pt.5/02-Map-pt.2.js
@@ -9,6 +9,7 @@
         - array completo
 */
 
+// Se for vip usa a pulseira preta
 const lista = [
     { nome: "Raphael", vip: true },
     { nome: "Victoria", vip: false },
@@ -28,3 +29,33 @@ const novaLista = lista.map( usuario => {
 })
 
 console.log(novaLista)
+console.log("\n") // espaçamento no terminal
+
+
+
+// abaixo de 5 está reprovado
+const estudantes = [
+    { nome: "Raphael", nota: 8 },
+    { nome: "Rodolfo", nota: 7 },
+    { nome: "Viviane", nota: 4 },
+    { nome: "Andressa", nota: 6},
+    { nome: "Danilo", nota: 2 },
+    { nome: "Beatriz", nota: 9 },
+]
+
+const verificacao = estudantes.map(estudante => {
+    if (estudante.nota >= 5) {
+        nota = estudante.nota + " -> Aprovado"
+    } else {
+        nota = estudante.nota + " -> Reprovado"
+    }
+
+    const verificando = {
+        nome: estudante.nome,
+        nota: nota
+    }
+    
+    return verificando
+})
+
+console.log(verificacao)
