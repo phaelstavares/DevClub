7b9ba8df54d94041c91be5709066828d4cc1fa85
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-23 16:57:38 -0300

    Desafio 2 - Map, Reduce & Filter

--- JavaScript/JavaScript pt.5/05-Desafio-MapReduceFilter.js
+++ JavaScript/JavaScript pt.5/05-Desafio-MapReduceFilter.js
@@ -7,9 +7,12 @@
 */
 
 const companhias = [
-    {nome: "Samsung", valorMercado: 50, ceo: "Kim Hyun Suk", fundacao: 1938},
-    {nome: "Apple", valorMercado: 845, ceo: "Tim Cook", fundacao: 1976},
-    {nome: "Spotify", valorMercado: 300, ceo: "Daniel EK", fundacao: 2006},
+    { nome: 'Intel', valorMercado: 117, ceo: 'Brian Krzanich', fundacao: 1968 },
+    { nome: 'Samsung', valorMercado: 50, ceo: 'Kim Hyun Suk', fundacao: 1938 },
+    { nome: 'Microsoft', valorMercado: 415, ceo: 'Satya Nadella', fundacao: 1975 },
+    { nome: 'Facebook', valorMercado: 383, ceo: 'Mark Zuckerberg', fundacao: 2004 },
+    { nome: 'Apple', valorMercado: 845, ceo: 'Tim Cook', fundacao: 1976 },
+    { nome: 'Spotify', valorMercado: 30, ceo: 'Daniel Ek', fundacao: 2006 }
 ]
 
 // Em vez de criar várias variáveis, posso criar uma só e ir linkando o 'Filter' e 'Reduce'.
@@ -27,3 +30,30 @@ const compMap = companhias.map(valor => {
 }, 0) // valor inicial do acumulador
 
 console.log(compMap)
+console.log("\n") // espaçamento no terminal
+
+
+
+/*
+    Desafio 2
+
+    Subtrair 10% do valor de mercado a todas as companhias -> MAP
+    Filtrar somente companhias fundadas depois de 1980 -> FILTER
+    Soma o valor de mercado das restantes -> REDUCE
+*/
+
+const novasCompanhias = companhias.map(valor => {
+    valor.valorMercado = valor.valorMercado.toFixed() - (valor.valorMercado / 10)
+    // toFixed() é uma ferramenta interna do JavaScript que ela vai falar quantas casas decimais eu quero.
+    
+    return valor
+    
+}).filter(datafundacao => {
+    return datafundacao.fundacao > 1980
+
+}).reduce((acumulador, valorTotal) => {
+    return acumulador + valorTotal.valorMercado
+
+}, 0) // valor inicial do acumulador
+
+console.log(novasCompanhias)
