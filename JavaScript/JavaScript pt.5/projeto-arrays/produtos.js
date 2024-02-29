22e84416d608a40eaf4ec97582845c1109ba7109
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-29 16:09:36 -0300

    javascript

--- JavaScript/JavaScript pt.5/projeto-arrays/produtos.js
+++ JavaScript/JavaScript pt.5/projeto-arrays/produtos.js
@@ -1,5 +1,5 @@
 const menuOptions = [
-    { name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.jpeg' },
+    { name: 'X-Salada', price: 30, vegan: false, src: './img/xsalada.png' },
     { name: 'X-Bacon', price: 34, vegan: false, src: './img/xbacon.png' },
     { name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/bacon-egg.png' },
     { name: 'Monstruoso', price: 50, vegan: false, src: './img/monstruoso.png' },
---
+++ JavaScript/JavaScript pt.5/projeto-arrays/scripts.js
@@ -0,0 +1,5 @@
+const mainVisivel = document.querySelector("main")
+
+function visivel() {
+    mainVisivel.style.visibility = "visible"
+}
