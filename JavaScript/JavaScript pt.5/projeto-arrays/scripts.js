77a761425933c529a55391434e02f827cefcf38b
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-03-01 17:44:59 -0300

    for each

--- JavaScript/JavaScript pt.5/projeto-arrays/scripts.js
+++ JavaScript/JavaScript pt.5/projeto-arrays/scripts.js
@@ -1,5 +1,19 @@
-const mainVisivel = document.querySelector("main")
+const lista = document.querySelector("ul")
+const botaoMostrarTudo = document.querySelector(".botaoMostrarTudo")
+let meuLi = ""
 
-function visivel() {
-    mainVisivel.style.visibility = "visible"
+function mostrarTudo() {
+    menuOptions.forEach(produto => {
+        meuLi +=  /* meuLi = meuLi + */ `
+            <li>
+                <img src=${produto.src} alt="${produto.name}">
+                <p>${produto.name}</p>
+                <p class="preço-item">R$ ${produto.price}</p>
+            </li>
+        `
+    })
+    
+    lista.innerHTML = meuLi
 }
+
+botaoMostrarTudo.addEventListener("click", mostrarTudo)
