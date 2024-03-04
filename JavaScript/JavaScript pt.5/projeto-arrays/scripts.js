a37b43e674ad2a808d10ee9299174d015266bcb5
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-03-04 20:01:35 -0300

    reduce e filter

--- JavaScript/JavaScript pt.5/projeto-arrays/scripts.js
+++ JavaScript/JavaScript pt.5/projeto-arrays/scripts.js
@@ -1,6 +1,14 @@
 const lista = document.querySelector("ul")
 const botaoMostrarTudo = document.querySelector(".botaoMostrarTudo")
 const botaoDesconto = document.querySelector(".adicionar-desconto")
+const somarTudo = document.querySelector(".somar-tudo")
+const filtrarTudo = document.querySelector(".filtrar-tudo")
+
+function formatarDinheiro(valor) {
+    const novaMoeda = valor.toLocaleString("pt-br", {style: "currency", currency: "BRL"});
+
+    return novaMoeda
+}
 
 function mostrarTudo(produtosArray) {
     let meuLi = ""
@@ -10,7 +18,7 @@ function mostrarTudo(produtosArray) {
             <li>
                 <img src=${produto.src} alt="${produto.name}">
                 <p>${produto.name}</p>
-                <p class="preço-item">R$ ${produto.price}</p>
+                <p class="preço-item"> ${formatarDinheiro(produto.price)}</p>
             </li>
         `
     })
@@ -27,5 +35,25 @@ function mostrarDesconto() {
     mostrarTudo(novosPrecos)
 }
 
+function SomarTodosItens() {
+    const valorTotal = menuOptions.reduce((acumulador, valorAtual) => acumulador + valorAtual.price, 0)
+    console.log(valorTotal)
+
+
+    lista.innerHTML = `
+        <li>
+            <p>O valor total dos itens do menu é <span class="preço-item"> ${formatarDinheiro(valorTotal)} </span></p>
+        </li>
+    `
+}
+
+function filtrarTudoVegano() {
+    const filtrarSoVegano = menuOptions.filter(produto => produto.vegan)
+    
+    mostrarTudo(filtrarSoVegano)
+}
+
 botaoMostrarTudo.addEventListener("click", () => mostrarTudo(menuOptions))
 botaoDesconto.addEventListener("click", mostrarDesconto)
+somarTudo.addEventListener("click", SomarTodosItens)
+filtrarTudo.addEventListener("click", filtrarTudoVegano)
