353bc69af6e17be3cfe41db9599bc6e4b97446b3
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-04-08 15:19:59 -0300

    estilizando

--- React/React pt.1/primeiro-projeto-react/src/App.js
+++ React/React pt.1/primeiro-projeto-react/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container, Imagem, ContainerItens, H1, InputLabel, Input, Button } from "./styles"
+import People from "./assets/img-pessoas-inicio.svg"
+import Seta from "./assets/seta-direita.svg"
 
 // JSX
 export default function App ()  {
 
   return (
     <Container>
-      <Imagem />
+      <Imagem alt="logo imagem" src={People}/>
         <ContainerItens>
 
           <H1>Olá!</H1>
@@ -17,7 +19,7 @@ export default function App ()  {
           <InputLabel>Idade</InputLabel>
           <Input placeholder="Idade"></Input>
 
-          <Button>Cadastrar</Button>
+          <Button>Cadastrar <img alt="seta" src={Seta}/></Button>
 
         </ContainerItens>
     </Container>
