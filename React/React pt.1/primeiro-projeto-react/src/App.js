d72244f46016e175db67059fa2c3e02486d2a09f
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-04-10 14:35:50 -0300

    pt.1

--- React/React pt.1/primeiro-projeto-react/src/App.js
+++ React/React pt.1/primeiro-projeto-react/src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
-import { Container, Imagem, ContainerItens, H1, InputLabel, Input, Button } from "./styles"
+import React, { useState, useRef } from "react";
+import { Container, Imagem, ContainerItens, H1, InputLabel, Input, Button, User } from "./styles"
 import People from "./assets/img-pessoas-inicio.svg"
 import Seta from "./assets/seta-direita.svg"
+import Lixeira from "./assets/lixeira.svg"
 
 // JSX
 export default function App ()  {
+  // React Hooks -> Ferramentas auxiliares
+  const [ users, setUsers ] = useState([])
+  const inputNome = useRef()
+  const inputIdade = useRef()
+
+  function adicionarUser() {
+    setUsers([...users,{id: Math.random(), nome: inputNome.current.value, idade: inputIdade.current.value}])
+    // spread operator (...)
+  }
+
+  function excluirUser(userId) {
+    const NovoUsers = users.filter(user => user.id !== userId)
+    
+    setUsers(NovoUsers)
+  }
 
   return (
     <Container>
@@ -14,13 +30,26 @@ export default function App ()  {
           <H1>Olá!</H1>
 
           <InputLabel>Nome</InputLabel>
-          <Input placeholder="Nome"></Input>
+          <Input ref={inputNome} placeholder="Nome"></Input>
 
           <InputLabel>Idade</InputLabel>
-          <Input placeholder="Idade"></Input>
+          <Input ref={inputIdade} placeholder="Idade"></Input>
 
-          <Button>Cadastrar <img alt="seta" src={Seta}/></Button>
+          <Button onClick={adicionarUser}>Cadastrar <img alt="seta" src={Seta}/></Button>
 
+          <ul>
+            { users.map(user => (
+              <User key={user.id}>
+
+              <p>{user.nome}</p> <p>{user.idade}</p>
+
+              <button onClick={() => excluirUser(user.id)}>
+                <img src={Lixeira} alt="lixeira"/>
+              </button>
+              </User>
+            ))
+            }
+          </ul>
         </ContainerItens>
     </Container>
   )
