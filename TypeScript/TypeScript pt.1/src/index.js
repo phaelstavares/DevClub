4883b4c60b4cfd435585ed99bee8f1cf835b04bd
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-12-18 23:41:49 -0300

    aula 1 - montando o setup

---
+++ TypeScript/TypeScript pt.1/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+function sum(a, b) {
+    return (a + b);
+}
+const result = sum(4, 5);
+console.log("O resultado da soma é:", result);
---
+++ TypeScript/TypeScript pt.1/src/index.ts
@@ -0,0 +1,6 @@
+function sum(a: number, b: number): number { // somar dois números e retornar um número
+    return (a + b);
+}
+
+const result = sum(4, 5);
+console.log("O resultado da soma é:", result);
